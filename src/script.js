935cf17822e22322997ec4857d874792f248c9e7
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-11-23 19:39:28 +0100

    add custom uniforms

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 import GUI from 'lil-gui';
 import testVertexShader from './shaders/test/vertex.glsl';
 import testFragmentShader from './shaders/test/fragment.glsl';
@@ -29,7 +29,7 @@ const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
 
 const count = geometry.attributes.position.count;
 const randoms = new Float32Array(count);
-for(let i = 0; i < count; i++) {
+for (let i = 0; i < count; i++) {
     randoms[i] = Math.random();
 }
 geometry.setAttribute('aRandom', new THREE.BufferAttribute(randoms, 1));
@@ -38,11 +38,19 @@ geometry.setAttribute('aRandom', new THREE.BufferAttribute(randoms, 1));
 const material = new THREE.RawShaderMaterial({
     vertexShader: testVertexShader,
     fragmentShader: testFragmentShader,
-    transparent: true
+    uniforms: {
+        uFrequency: { value: new THREE.Vector2(10,5) },
+        uTime: { value: 0 },
+        uColor: { value: new THREE.Color('orange') },
+    }
 })
+
+gui.add(material.uniforms.uFrequency.value, 'x').min(0).max(20).step(0.01).name('frequencyX');
+gui.add(material.uniforms.uFrequency.value, 'y').min(0).max(20).step(0.01).name('frequencyY');
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material);
+mesh.scale.y = 2/3;
 scene.add(mesh);
 
 /**
@@ -53,8 +61,7 @@ const sizes = {
     height: window.innerHeight
 };
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth;
     sizes.height = window.innerHeight;
@@ -73,7 +80,7 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
-camera.position.set(0.25, - 0.25, 1);
+camera.position.set(0.25, -0.25, 1);
 scene.add(camera);
 
 // Controls
@@ -94,9 +101,11 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  */
 const clock = new THREE.Clock();
 
-const tick = () =>
-{
+const tick = () => {
     const elapsedTime = clock.getElapsedTime();
+
+    // Update material
+    material.uniforms.uTime.value = elapsedTime;
 
     // Update controls
     controls.update();
